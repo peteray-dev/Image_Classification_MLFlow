0c3a1a6bba04091fd8bbf75a6d2202705ac15175
peteray-dev <peteraydev@gmail.com>
2025-02-04 20:03:52 +0000

    data evaluation created and model moving to mlflow for experimentation

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 import os 
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig
+from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig, EvaluationConfig
 class ConfigurationManager:
     def __init__(self, config_path=CONFIG_FILE_PATH, param_path=PARAM_FILE_PATH):
         self.config = read_yaml(config_path)
@@ -64,3 +64,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Training",
+            mlflow_uri="https://dagshub.com/peteray-dev/Image_Classification_MLFlow.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
